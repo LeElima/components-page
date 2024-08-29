f3de91b7adab7ac013fb6cefaf921ae25911f0d5
Leandro de Lima <leandrolima@cobrape.com.br>
2024-08-29 18:18:46 -0300

    Melhorado layout e funcionamento da DataTable

--- src/views/DataTableView.vue
+++ src/views/DataTableView.vue
@@ -1,8 +1,23 @@
 <template>
     <div class="about">
-      <data-table :headers="headers" :items="items" />
+        <h1>DataTable</h1>
+        <div class="tabela">
+            <data-table :headers="headers" :items="items" />
+        </div>
+      
     </div>
   </template>
+  <style>
+    .about{
+        display: flex;
+    flex-direction: column;
+    }
+    .tabela{
+        display: flex;
+        justify-content: center;
+        
+    }
+</style>
 <script lang="ts">
 import Vue from 'vue';
 import DataTable from '@/components/DataTable.vue';
@@ -14,10 +29,30 @@ export default Vue.extend({
         return{
             headers: [
                 {
-                    text: "Descrição"
-                }
+                    text: "Nome",
+                    value: "nome"
+                },
+                {
+                    text: "Idade",
+                    value: "idade"
+                },
+                {
+                    text: "Profissão",
+                    value: "profissao"
+                },
             ],
-            items: []
+            items: [
+                {
+                    nome: "Leandro",
+                    idade: "28",
+                    profissao: "Programador"
+                },
+                {
+                    nome: "João",
+                    idade: "60",
+                    profissao: "Aposentado"
+                },
+            ]
         }
     },
 })
